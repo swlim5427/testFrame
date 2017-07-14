a20b46dc4ab372c597a5e98d8e96daeeeaf8c553
swlim <swlim@163.com>
2017-07-14 19:10:01 +0800

    2017-7-13

--- commonFunc/public_methods.py
+++ commonFunc/public_methods.py
@@ -1,9 +1,11 @@
+# coding=utf-8
 import threading
 import sqlite3
 import test_frame_distribute
+import time
 
 def sqliteConnect(sql):
-    conn = sqlite3.connect('test.db')
+    conn = sqlite3.connect('testFrame.db')
     if sql[1] == 2:
         conn.close()
     else:
@@ -19,26 +21,35 @@ def sqliteConnect(sql):
             print e
             return False
 
-def checkSql():
-    creatTableSql = "create table testFrame (id text,caseId text,nextId text);"
-    insertCheckTableSql = "insert into testFrame VALUES (100000,\"\",\"\")"
-
-    sqlList=[creatTableSql,insertCheckTableSql]
-    return sqlList
-
-def correspondence(caseId,nextId):
-    logicList = [caseId,nextId]
-
-
-def checkTable():
-    countSql = "select count(*) from testFrame"
-    countRes = sqliteConnect([countSql,0])
-    return countRes
-
 def initTable():
-    dSql = "delete from testFrame"
-    sqliteConnect([dSql,1])
 
+    cSql = "create table testFrame (id text,caseId text,nextId text,count text,times text,nowCaseId text,leftNextId text);"
+    # id:执行id
+    # caseId:启动测试的用例id
+    # nextid:本次除caseId外所需要执行的caseId
+    # count:当前执行到第几次
+    # times:总计要执行多少次
+    # nowCaseId:当前执行的caseId
+    # leftNextId:剩余要执行的caseId
+    sqliteConnect([cSql, 0])
+
+    countSql = "select count(*) from testFrame;"
+    iSql = "insert into testFrame VALUES (\"1000001\",\"\",\"\",\"\",\"\",\"\",\"\");"
+
+    try:
+        for ida in sqliteConnect([countSql,0]):
+            lineCount = ida[0]
+        if lineCount == 0:
+
+            sqliteConnect([iSql, 0])
+        # else:
+        #     dSql = "drop table testFrame;"
+        #     sqliteConnect([dSql, 0])
+
+        # getNextCaseId(ida[0])
+
+    except Exception as e:
+        print e
 
 def createThreading(param):
 
@@ -53,5 +64,13 @@ def stopThreading():
     threading._shutdown()
     print "1"
 
-def callback():
-    print "callback"
+def callback(message,testId):
+
+    sSql = "select * from testFrame where id ="+str(testId)+";"
+    for r in sqliteConnect([sSql,0]):
+        nowCaseId = r[5]
+
+
+    for i in range(1):
+        # t = threading.current_thread().getName()
+        print "callback:  ",message,"-----",i+1,"-----",testId,"------"

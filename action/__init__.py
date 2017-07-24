63d25094fe4e3adfa783eeb46b556b0e529c2331
swlim <swlim@swlimdeMacBook-Pro.local>
2017-07-24 18:09:56 +0800

    rm

--- action/__init__.py
+++
@@ -1,3 +0,0 @@
-__all__ = ["asrtest"]
-def actionType():
-    return __all__
--- commonFunc/__init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = ["test_frame_distribute",
-           "public_methods",
-           "check_test"]
-def actionType():
-    return __all__

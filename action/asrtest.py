7def333b916a4cbaed5fe540708c0295c8a4d31c
swlim <swlim@swlimdeMacBook-Pro.local>
2017-07-25 15:48:16 +0800

    add ratefile

--- action/asrtest.py
+++ action/asrtest.py
@@ -22,7 +22,7 @@ class AsrTest(object):
         self.decoderConfig = self.data["config"]
         self.runTime = commonfunc.public_methods.get_dateTime(1, 0)
 
-        self.testPath = self.data["config"].split("decoder.conf")[0]
+
         #
         # self.asrResult = self.testPath,self.caseId+"-",self.testId,"/result/",self.runTime
         # self.asrLog = self.testPath,"log/",self.runTime,".log"
@@ -35,9 +35,12 @@ class AsrTest(object):
 
         pwd = os.getcwd()
         testFolder = self.version+"-"+self.caseId+"-" + self.testId
-        resultPath, logPath = commonfunc.public_methods.mkdir(self.testPath, testFolder)
+        testPath = self.data["config"].split("decoder.conf")[0]
+        answerPath = self.testCase["answer"].split("answer.list")[0]
 
-        casePath = self.testPath+testFolder
+        resultPath, logPath = commonfunc.public_methods.mkdir(testPath, testFolder)
+
+        casePath = testPath+testFolder
         os.chdir(pwd)
 
         if self.url != "":
@@ -49,8 +52,12 @@ class AsrTest(object):
         lResult = str(resultPath) + "/" + str(self.runTime) + ".log"
         sResult = str(resultPath) + "/" + str(self.runTime)
 
-        print self.caseId, "do test----",self.testId, "-----", threading.current_thread().getName()
+        # print self.caseId, "do test----",self.testId, "-----", threading.current_thread().getName()
         os.system("./"+str(self.testTools)+" --config "+str(self.decoderConfig)+" --filelist "+str(self.caseList)+" --log "+str(sResult)+" --sleep 2 >>"+lResult)
+        os.chdir(answerPath)
+        os.system("python ")
+
+        os.system("python pasr_calc_recrate.py -s sclite -m answer.list -r "+str(sResult)+" -o "+str(sResult)+"rate")
 
         os.chdir(pwd)
         time.sleep(1)
